959be9fb541e4e7a8b9ba4ccb6cc8139e387f50a
Felipe Marinho de Freitas <f.marinho162001@gmail.com>
2021-12-05 22:01:36 -0400

    feat: adiciona verificação de login

--- index.js
+++ index.js
@@ -10,62 +10,80 @@ require('dotenv').config()
 const writeStream = fs.createWriteStream('githubIssues.csv',{encoding: 'utf8'});
 writeStream.write(`Titulo; Descricão; Data de criação \n`);
 
+
+const loggedCheck = async (page) => {
+    try {
+        await page.waitForSelector(`[content=${process.env.LOGIN}]`, { timeout: 10000 });
+        return true;
+    } catch(err) {
+        return false;
+    }
+};
 //Start
 (async () => {
     //Config
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: [
             "--no-sandbox"
         ]
         
     });
+
+    let isLogged = false;
+
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(0)
-    await page.goto("https://github.com/login");
-    console.log("sucesso ao acessar a pagina principal");
-
 
     //Login
-    await page.type('#login_field', process.env.LOGIN);
-    await page.type('#password', process.env.PASSWORD);
-    await page.click('[type="submit"]');
-    await page.waitForNavigation();
-    console.log("sucesso ao logar");
+    if (!isLogged){
+        await page.goto("https://github.com/login");
+        console.log("sucesso ao acessar a pagina principal");
 
+        await page.type('#login_field', process.env.LOGIN);
+        await page.type('#password', process.env.PASSWORD);
+        await page.click('[type="submit"]');
+        await page.waitForNavigation();
 
-    //Track to issue list
-    await page.click('[href="/issues"]');
-    await page.waitForSelector('.Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title');
-    console.log("sucesso ao carregar pagina de issues");
-
-
-    //Load href of issues
-    const hrefs = await page.evaluate(
-        () => Array.from(
-          document.querySelectorAll('.Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title'),
-          a => a.getAttribute('href')
-        )
-    );
-
-
-    //Access issue page
-    for(const href of hrefs){
-        await page.goto('https://github.com' + href)
-        console.log('Acessado com sucesso a pagina: https://github.com' + href);
-        await page.waitForSelector('.js-issue-title.markdown-title');
-        const issuePage = await page.content();
-
-        //Get title, date and description with cheerio
-        const $ = cheerio.load(issuePage);
-        const title = $('.js-issue-title.markdown-title').text();
-        const date = $('relative-time').attr('datetime');
-        const description = $('[dir="auto"]').text();
-
-        writeStream.write(`${title}; ${description}; ${date} \n`);
-        
-        await page.goBack();
+        isLogged = await loggedCheck(page);
     }
-    
-    await browser.close(); 
+
+        
+    if (isLogged == true){
+        //Track to issue list
+        await page.click('[href="/issues"]');
+        await page.waitForSelector('.Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title');
+        console.log("sucesso ao carregar pagina de issues");
+
+
+        //Load href of issues
+        const hrefs = await page.evaluate(
+            () => Array.from(
+            document.querySelectorAll('.Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title'),
+            a => a.getAttribute('href')
+            )
+        );
+
+
+        //Access issue page
+        for(const href of hrefs){
+            await page.goto('https://github.com' + href)
+            console.log('Acessado com sucesso a pagina: https://github.com' + href);
+            await page.waitForSelector('.js-issue-title.markdown-title');
+            const issuePage = await page.content();
+
+            //Get title, date and description with cheerio
+            const $ = cheerio.load(issuePage);
+            const title = $('.js-issue-title.markdown-title').text();
+            const date = $('relative-time').attr('datetime');
+            const description = $('[dir="auto"]').text();
+
+            writeStream.write(`${title}; ${description}; ${date} \n`);
+            
+            await page.goBack();
+        }
+    } else {
+        throw new Error('Incorrect username or password.')
+    }
+    await browser.close();
 }) ().catch(err => console.log(err));
